890ae991437bc8ea991c57f3e067f6211c5a8fe9
williams29008 <60887276+williams29008@users.noreply.github.com>
2026-07-04 19:58:12 +0100

    Improve input validation in getKnots function
    Updated input validation to check for empty input before checking if it's a number.

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ async function getKnots() {
     const valueNumber = document.getElementById("valueNumber").value;
     const result = document.getElementById("result");
 
-    if (isNaN(valueNumber)) {
+    if (!valueNumber) {
         result.innerHTML = "Please enter a value";
         return;
     }
+    
+    if (isNaN(valueNumber)) {
+        result.innerHTML = "Please enter a valid number";
+        return;
+    }
+    
         const Knots = valueNumber * 0.868976;
         const MPH = valueNumber;
         const ms = valueNumber * 0.44704;
         
         result.innerHTML = `
             <h2>Results</h2>
-            <p><strong>MPH: ${MPH.toFixed(1)} </p>
-            <p><strong>Knots: ${Knots.toFixed(1)}</p>
-            <p><strong>M/S: ${ms.toFixed(1)}</p>
+            <p><strong>MPH: ${MPH.toFixed(1)}</strong></p>
+            <p><strong>Knots: ${Knots.toFixed(1)}</strong></p>
+            <p><strong>M/S: ${ms.toFixed(1)}</strong></p>
         `;
 
 }
