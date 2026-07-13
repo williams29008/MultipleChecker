31b78bb6314f6133c3b4d43a78110b86d79e7b40
williams29008 <60887276+williams29008@users.noreply.github.com>
2026-07-13 20:38:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,33 @@ async function getMPH() {
         `;
 
 }
+
+async function getMS() {
+    const valueInput = document.getElementById("valueNumber").value;
+    const result = document.getElementById("result");
+
+    // Empty check
+    if (valueInput === "") {
+        result.innerHTML = "Please enter a value";
+        return;
+    }
+
+    // Convert to number
+    const valueNumber = Number(valueInput);
+    
+    if (isNaN(valueNumber)) {
+        result.innerHTML = "Please enter a valid number";
+        return;
+    }
+								const MPH = valueNumber * 2.23694;
+        const ms = valueNumber;
+        const Knots = valueNumber * 1.94384;
+
+        result.innerHTML = `
+            <h2>Results</h2>
+            <p><strong>MPH: ${MPH.toFixed(1)}</strong></p>
+            <p><strong>Knots: ${Knots.toFixed(1)}</strong></p>
+            <p><strong>M/S: ${ms.toFixed(1)}</strong></p>
+        `;
+
+}
