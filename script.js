be605ab5d5d0aa5ec7af5cc1c61c7cada11c40b2
williams29008 <60887276+williams29008@users.noreply.github.com>
2026-07-16 18:37:23 +0100

    Refactor Beaufort Force calculation logic

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ async function getKnots() {
 			case (MPH <= 63):
 				F = 10;
 				break;
-			case (MPH <= 72):
+			case (MPH < 73):
 				F = 11;
 				break;
 			case (MPH >= 73):
@@ -96,12 +96,55 @@ async function getMPH() {
         const Knots = valueNumber;
         const MPH = valueNumber * 1.15078;
         const ms = valueNumber * 0.514;
-        
+        let F = 99;
+		switch (true) {
+			case (MPH < 1):
+				F = 0;
+				break;
+			case (MPH <= 3):
+				F = 1;
+				break;
+			case (MPH <= 7):
+				F = 2;
+				break;
+			case (MPH <= 12):
+				F = 3;
+				break;
+			case (MPH <= 18):
+				F = 4;
+				break;
+			case (MPH <= 24):
+				F = 5;
+				break;
+			case (MPH <= 31):
+				F = 6;
+				break;
+			case (MPH <= 38):
+				F = 7;
+				break;
+			case (MPH <= 46):
+				F = 8;
+				break;
+			case (MPH <= 54):
+				F = 9;
+				break;
+			case (MPH <= 63):
+				F = 10;
+				break;
+			case (MPH < 73):
+				F = 11;
+				break;
+			case (MPH >= 73):
+				F = 12;
+				break;
+		}
+
         result.innerHTML = `
             <h2>Results</h2>
             <p><strong>MPH: ${MPH.toFixed(1)}</strong></p>
             <p><strong>Knots: ${Knots.toFixed(1)}</strong></p>
             <p><strong>M/S: ${ms.toFixed(1)}</strong></p>
+			<p><strong>Beaufort Force ${F}</strong></p>
         `;
 
 }
@@ -126,12 +169,56 @@ async function getMS() {
 								const MPH = valueNumber * 2.23694;
         const ms = valueNumber;
         const Knots = valueNumber * 1.94384;
+		let F = 99;
+		switch (true) {
+			case (MPH < 1):
+				F = 0;
+				break;
+			case (MPH <= 3):
+				F = 1;
+				break;
+			case (MPH <= 7):
+				F = 2;
+				break;
+			case (MPH <= 12):
+				F = 3;
+				break;
+			case (MPH <= 18):
+				F = 4;
+				break;
+			case (MPH <= 24):
+				F = 5;
+				break;
+			case (MPH <= 31):
+				F = 6;
+				break;
+			case (MPH <= 38):
+				F = 7;
+				break;
+			case (MPH <= 46):
+				F = 8;
+				break;
+			case (MPH <= 54):
+				F = 9;
+				break;
+			case (MPH <= 63):
+				F = 10;
+				break;
+			case (MPH < 73):
+				F = 11;
+				break;
+			case (MPH >= 73):
+				F = 12;
+				break;
+		}
+
 
         result.innerHTML = `
             <h2>Results</h2>
             <p><strong>MPH: ${MPH.toFixed(1)}</strong></p>
             <p><strong>Knots: ${Knots.toFixed(1)}</strong></p>
             <p><strong>M/S: ${ms.toFixed(1)}</strong></p>
+			<p><strong>Beaufort Force ${F}</strong></p>
         `;
 
 }
